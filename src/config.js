9cff49c0713b68ee68d099c240536876844bab3e
harshavardh8n <harshavardhangaikwad39@gmail.com>
2025-01-02 17:31:07 +0530

    In production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const backendPath = "http://localhost:3001/"
 // export const backendPath = "https://rommie-mate-backend.vercel.app/"
-export const backendPath = "https://rommie-mate-bakend.vercel.app/"
-// export const backendPath = "http://localhost:3001/"
+// export const backendPath = "https://rommie-mate-bakend.vercel.app/"
+export const backendPath = "http://localhost:3001/"
